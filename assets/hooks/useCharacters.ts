6453a9361fe37fdd4be4517981a59dda7e865fec
Briws <brightwiths@gmail.com>
2024-03-28 21:47:56 +0700

    My first nest.js app by It-incubator lesson

---
+++ assets/hooks/useCharacters.ts
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react"
+import axios from "axios"
+import {Nullable} from "@/assets/types"
+
+export const  useCharacters = (): Nullable<CharacterType[]> => {
+    const [characters, setCharacters] = useState<Nullable<CharacterType[]>>(null)
+
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_RICK_AND_MORTY_API_URL}/character`)
+            .then(res => setCharacters(res.data.results))
+    })
+
+    return characters
+}
+
+//types
+export type CharacterType = {
+    id: number
+    name: string
+    image: string
+}
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import Link from "next/link"
+import styles from './Navbar.module.scss'
+
+ export const Navbar = () => {
+
+    return (
+        <div className={styles.links}>
+             <Link href={'/'}>Main</Link>
+             <Link href={'/characters'}>Characters</Link>
+        </div>
+    )
+}
